d5e322ce43af25578c82428c6c61f88d7e227dde
Devmithra Panwar Arya <dev87111@gmail.com>
2025-09-14 16:08:53 +0530

    added function for receiving placed orders from database to display in Orders.jsx

--- src/pages/AdminDashboard pages/Orders.jsx
+++ src/pages/AdminDashboard pages/Orders.jsx
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Orders() {
+
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        getOrders();
+    }, []);
+
+    async function getOrders() {
+        const response = await axios.get("http://localhost:8080/orders/getOrders");
+        setOrders(response.data);
+    }
+
     return (
         <>
             <h3>Pending Orders</h3> 
@@ -7,34 +22,26 @@ function Orders() {
                     <thead> 
                         <tr className="table-header"> 
                             <th scope="col">S.no.</th> 
-                            <th scope="col">Name</th> 
-                            <th scope="col">Payment Mode</th> 
-                            <th scope="col">Product_id</th> 
-                            <th scope="col">Order State</th>
-                            <th scope="col">Image</th>
-                            <th scope="col">Actions</th> 
+                            <th scope="col">Customer Name</th> 
+                            <th scope="col">Product Name</th> 
+                            <th scope="col">Product Id</th> 
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Pincode</th> 
                         </tr> 
                     </thead> 
                     <tbody>
-                        <tr>
-                            <td>Pending Orders will be shown here....</td>
-                        </tr>
-                        {/* 
-                        {products.map((product, index) => (
-                        <tr key={product.id}>
+                        {orders.map((order, index) => (
+                        <tr key={orders.id}>
                             <td>{index + 1}</td>
-                            <td>{product.name}</td>
-                            <td>{product.price}</td>
-                            <td>{product.stock}</td>
-                            <td>{product.desc}</td>
-                            <td><img src={product.img} alt={product.name} width="80" /></td>
-                            <td>      
-                                <button className="btn btn-sm btn-info mr-2" onClick={() => handleEdit(product)}>Edit</button>
-                                <button className="btn btn-sm btn-danger" onClick={() => handleDelete(product.id)}>Delete</button>
-                            </td>
+                            <td>{order.customerName}</td>
+                            <td>{order.productName}</td>
+                            <td>{order.productId}</td>
+                            <td>{order.quantity}</td>
+                            <td>{order.address}</td>
+                            <td>{order.pincode}</td>
                         </tr>
                         ))}
-                        */}
                     </tbody>
                 </table> 
             </div> 
